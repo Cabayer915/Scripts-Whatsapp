476fa467c5a20ff4207a3c0e1fb0bcc79bc41434
Cabayer915 <cauaribeirob915@gmail.com>
2024-03-26 13:54:43 -0300

    Adicionando lances

--- GolsCR7.js
+++ GolsCR7.js
@@ -45,4 +45,54 @@ Esse monstro
 Esse jogador inexplicável
 Chamado
 Cristiano Ronaldo
+
+O Cristiano Ronaldo, 
+ele ta carregando Solskjaer, 
+ele ta carregando Pogba, 
+ele ta carregando Rashford, 
+ele ta carregando a família Gleizer, 
+o Cristiano Ronaldo ta carregando o Old Trafford, 
+ele ta carregando tudo sozinho. 
+
+ELE TA BOTANDO ESSA CHAMPIONS NO BOLSO! 
+Essa Champions é de Cristiano Ronaldo, 
+ele pegou a fase de grupos da Champions e colocou no bolso. 
+"EU RESOLVO." 
+"SE NINGUÉM FAZ NADA, 
+SE NÃO TEM TRABALHO TÁTICO, 
+SE NÃO TEM COLETIVO, 
+SE NÃO TEM FUTEBOL, 
+EU RESOLVO." 
+"EU SOU O SENHOR CHAMPIONS" 
+"UEFA, CRISTIANO RONALDO LEAGUE". 
+É isso e acabou!
+
+É de um protagonista que o Time do Povo precisa? 
+Aí está, Yuri Alberto. 
+Sem se esconder. 
+Sem se omitir. 
+Pegou a bola
+Botou debaixo do braço e foi pra cobrança de pênalti. 
+É gol do Corinthians!
+
+Incrível e horrível cobrança. 
+Uma das piores cobranças que eu já vi de pênalti na minha vida. 
+Uma cobrança simplesmente lamentável. 
+Uma cobrança absurda, ridícula. 
+Uma cobrança simplesmente horrorosa desse jogador tão experiente. 
+Ele recuou a bola para o goleiro Thiago Couto. 
+E ele que já tinha perdido um gol na frente do goleiro do Juventude. 
+Agora cobra de forma ridícula, 
+absurda, 
+inexplicável e simplesmente absurda cobrança 
+Desse tão experiente jogador Vagner Love.
+
+"Esses negros maravilhosos, 
+que saem tabelando, 
+tocando".
+
+E lá vem eles de novo! 
+Olha só que absurdo!
+Ih lá vem mais... E LÁ VEM MAIS... olha a bola tocada... 
+VIROU PASSEIO!!!!
 `).then(e => console.log(`Código finalizado, ${e} mensagens enviadas`)).catch(console.error)
